60e03fae65c6a8ff2f3245e48bb288ade12b3749
newmancu <newmancu@gmail.com>
2025-04-10 02:47:35 +0300

    feat: Add base models

--- app_name/config.py
+++ app_name/config.py
@@ -6,7 +6,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from app_name.core.logs import init_logger
 
-levels = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+levels = ("TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+
+
+class PostgresSettings(BaseSettings):
+    user: str = "dcs_user"
+    password: str = "dcs_password"
+    db: str = "dcs_db"
+
+
+class DbSettings(BaseSettings):
+    host: str = "localhost"
+    port: str = "5432"
+    pool_size: int = 1  # just for async_session context
+
+    driver_schema: str = "postgresql+asyncpg"
 
 
 class AppSettings(BaseSettings):
@@ -22,6 +36,18 @@ class Settings(BaseSettings):
         env_file=(".env", ".prod.env"), env_file_encoding="utf-8"
     )
     log: LoggingSettings = Field(default_factory=LoggingSettings)
+
+    postgres: PostgresSettings = Field(default_factory=PostgresSettings)
+    db: DbSettings = Field(default_factory=DbSettings)
+
+    @property
+    def db_url(self) -> str:
+        return (
+            f"{self.db.driver_schema}://"
+            f"{self.postgres.user}:{self.postgres.password}@"
+            f"{self.db.host}:{self.db.port}"
+            f"/{self.postgres.db}"
+        )
 
 
 @cache
